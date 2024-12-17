4d823522f01c3ebcb66919ba4a4f7e0d1536b5db
Mouha <kmouhamadou517@gmail.com>
2024-12-17 20:27:47 +0100

    message modifier dans index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 < ?php 
  // Fonction principale pour afficher le message 
 function main() { 
- echo "Hello, GitHub!"; 
+ echo "Je me nomme Mouha Kane , le message a bien ete modifie"; 
 } 
 // Appel de la fonction principale 
 main(); 
